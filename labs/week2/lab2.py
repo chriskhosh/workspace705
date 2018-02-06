2e7bce64c7dc936b3f1de6cd4a7ff883c0280515
chriskhosh <ckhoshatefeh@gmail.com>
2018-02-06 10:45:10 -0500

    Lab 2 commit

--- labs/week2/lab2.py
+++ labs/week2/lab2.py
@@ -30,3 +30,61 @@ def check_title(title_list):
             list_title.append(x)
 
     return(list_title)
+
+def restock_inventory(inventory):
+    """
+    Increases inventory of each item in dictionary by 10
+
+    inventory: a dictionary with:
+
+        key: string that is the name of the inventory item
+
+        value: integer that equals the number of that item ccheck_title(urrently on hand
+
+    Returns: updated dictionary where each inventory item is restocked
+    """
+    inventory_dict = inventory
+
+    for x in inventory_dict:
+        inventory_dict.update({x : (inventory_dict.get(x) + 10)})
+
+    return(inventory_dict)
+
+def filter_0_items(inventory):
+    """
+    Removes items that have a value of 0 from a dictionary of inventories
+
+    inventory: dictionary with:
+
+        key: tring that is the name of the inventory item
+
+        value: nteger that equals the number of that item currently on hand
+
+    Returns: the same inventory_dict with any item that had 0 quantity removed
+    """
+    inventory_dict = {}
+
+    for x in inventory:
+        if inventory.get(x) != 0:
+            inventory_dict.update({x : (inventory.get(x))})
+
+    return(inventory_dict)
+
+def average_grades(grades):
+    """
+    Takes grade values from a dictionary and averages them into a final grade
+
+    grades: a dictionary of grades with:
+
+        key: string of students name
+
+        value: list of integer grades received in class
+
+    Returns: dictionary that averages out the grades of each student
+    """
+    grades_dict = {}
+
+    for x in grades:
+        grades_dict[x] = (sum(grades[x]) / float(len(grades[x])))
+
+    return(grades_dict)
