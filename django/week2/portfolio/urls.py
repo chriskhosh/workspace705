ec38c150fabf77ee4e9b9e7ec7055d74563c167b
chriskhosh <ckhoshatefeh@gmail.com>
2018-02-06 20:26:26 -0500

    Django week2

--- django/week2/portfolio/urls.py
+++ django/week2/portfolio/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='home'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^resume/', views.resume, name='resume'),
+    url(r'^portfolio/', views.portfolio, name='portfolio'),
+    url(r'^contact/', views.contact, name='contact'),
 ]
